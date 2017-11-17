5f1265e44efe7aebec906d26511f6e45889b63a7
tanling <490472172@qq.com>
2017-11-17 16:26:19 +0800

    remove sth

--- frontEnd/src/reducers/index.js
+++ frontEnd/src/reducers/index.js
@@ -2,13 +2,8 @@
  * reducers里的方法一般需要满足 functional program的要素，仅作数据的中间处理
  * index as reducers的集散中心
  */
-// import { combineReducers } from 'redux'
-import sort from './sort'
-import price from './price'
 
 const rootReducer = {
-  sort,
-  price
 }
 
 export default rootReducer
